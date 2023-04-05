178ab7213c3895d73622e4b32d9e31e4b5e09462
ris04hit <ris04hit@gmail.com>
2023-04-06 03:49:04 +0900

    minor fixed, downloaded mysqldb

--- backend/environments/app.py
+++ backend/environments/app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template
-import sqlite3
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+import MySQLdb.cursors
 
 app = Flask(__name__)
 
 @app.route('/')
 def start():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
     return render_template('html/start.html')
 
 @app.route('/js')
@@ -58,6 +50,10 @@ def home_js():
 @app.route('/institute')
 def institute():
     return render_template('html/institute.html')
+
+@app.route('/js/institute')
+def institute_js():
+    return render_template('js/institute.js')
 
 @app.route('/load/home')
 def loader1():
