378682ac3dfc4ccdb485804cee1ddf085c765a40
ris04hit <ris04hit@gmail.com>
2023-04-07 12:51:16 +0900

    signup implemented in backend

--- backend/environments/app.py
+++ backend/environments/app.py
@@ -12,7 +12,7 @@ app.config["MYSQL_DB"] = "Synergy_db"
 mysql = MySQL(app)
 
 otp = {}
-
+otp_signup = {}
 
 
 @app.route('/')
@@ -28,7 +28,7 @@ def start():
 def start_js():
     return render_template('js/start.js')
 
-@app.route('/main_js')
+@app.route('/js/main')
 def main_js():
     return render_template('js/main.js')
 
@@ -52,7 +52,7 @@ def create_institution_js():
 def header():
     return render_template('html/header.html')
 
-@app.route('/header.js')
+@app.route('/js/header')
 def header_js():
     return render_template('js/header.js')
 
@@ -62,10 +62,11 @@ def home(username):
         cur=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     except:
         print("Can not connect to database in start page")
-    query = "SELECT * from Account WHERE username = %s"
+    query = "SELECT * from Account WHERE BINARY username = %s"
     cur.execute(query,(username,))
-    user = cur.fetchall()
-    return render_template('html/home.html', user = user)
+    user = cur.fetchall()[0]
+    feed = helper.feed(cur, user)
+    return render_template('html/home.html', user = user, feed = feed)
 
 @app.route('/js/home')
 def home_js():
@@ -152,14 +153,14 @@ def login_form():
     if request.method == "POST":
         username = request.form["inp_start1"]
         password = request.form["inp_start2"]
-        query = "SELECT id_obj, id_uniq,  username FROM Account WHERE username = %s"
+        query = "SELECT id_obj, id_uniq,  username FROM Account WHERE BINARY username = %s"
         cur.execute(query, (username,))
         user = cur.fetchall()
         msg = ""
         if user:
             id_obj = user[0]['id_obj']
             id_uniq = user[0]['id_uniq']
-            query = "SELECT pass FROM Personal WHERE id_obj = %s AND id_uniq = %s"
+            query = "SELECT pass FROM Personal WHERE BINARY id_obj = %s AND BINARY id_uniq = %s"
             cur.execute(query, (id_obj, id_uniq))
             ids = cur.fetchall()
             cur.close()
@@ -187,7 +188,9 @@ def login_otp_form():
                     msg = "Enter OTP"
                     global otp
                     otp[email] = helper.gen_otp()
-                    print(otp) #instead send mail function
+                    otp_sub = str(otp[email])+" is your OTP for Synergy Login"
+                    otp_body = otp_sub
+                    helper.mail(email,otp_sub, otp_body)
                     cur.close()
                     return render_template("html/start.html", message1="", message2=msg, show_forget=True, otp_email = email)
                 msg = "Email not Registered, Please SignUp"
@@ -202,6 +205,7 @@ def login_otp_form():
                         cur.execute(query, (email,))
                         user = cur.fetchall()
                         username = user[0]['username']
+                        del otp[email]
                         cur.close()
                         return redirect("/home/"+username)
                     else:
@@ -214,6 +218,88 @@ def login_otp_form():
                     return render_template("html/start.html", message1 = "", message2 = msg, show_forget = True)
             cur.close()
             return render_template("html/start.html", message1 = "", message2 = "", show_forget=False)
+
+@app.route('/signup', methods = ['POST'])
+def signup_form():
+        try:
+            cur=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        except:
+            print("Can not connect to database in start page")
+        if request.method == "POST":
+            username = request.form['username_signup']
+            email = request.form['email_signup']
+            otp = request.form['otp_signup']
+            password = request.form['password_signup']
+            conf_password = request.form['confirm_signup']
+            country = request.form['country_signup']
+            state = request.form['state_signup']
+            city = request.form['city_signup']
+            website = request.form['web_signup']
+            git = request.form['git_signup']
+            org = request.form['org_signup']
+            image = request.form['image_profile_signup']
+            name = request.form['name_signup']
+            if request.form['send_button_signup'] == 'send_otp_signup':
+                if not helper.check_username(username):
+                    msg = "Username can only contain alphabets, number and _"
+                if not helper.unique_username(cur, username):
+                    msg = "Username already exists"
+                elif not email:
+                    msg = "Please enter Email"
+                else:
+                    msg = "Enter OTP"
+                    global otp_signup
+                    otp_signup[email] = helper.gen_otp()
+                    otp_sub = str(otp_signup[email])+" is your OTP for Synergy Signup"
+                    otp_body = "Hi "+username+"\nWelcome to Synergy,\n"+otp_sub
+                    helper.mail(email,otp_sub, otp_body)
+                cur.close()
+                return render_template("html/signup.html", message1=msg, username=username, email=email, country=country, state=state, city=city, website=website, git=git, org=org, image=image, name=name)
+            elif request.form['send_button_signup'] == 'create_account_signup':
+                if email in otp_signup:
+                    if otp == otp_signup[email]:
+                        if password != conf_password:
+                            msg = "Password and Confirm Password do not match"
+                            cur.close()
+                            return render_template("html/signup.html", message1=msg, username=username, email=email, country=country, state=state, city=city, website=website, git=git, org=org, image=image, otp=otp, name=name)
+                        elif helper.password_strength(password)!=1:
+                            msg = "Password strength is low"
+                            cur.close()
+                            return render_template("html/signup.html", message1=msg, username=username, email=email, country=country, state=state, city=city, website=website, git=git, org=org, image=image, otp=otp,name=name)
+                        hash_password = helper.hash_pwd(password)
+                        id_uniq = helper.id_gen(cur, 'A', username, "Account")
+                        query = "insert into Account (id_obj, id_uniq, username, name, email_id, institutes, posts, upvotes, bookmarks, followers, following, comments, visited_post, activity, tag_list, country, state, city, website_address, github_handle, organisation, visibility, report_list, api_visibility) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                        cur.execute(query,('A',id_uniq, username, name, email, id_uniq+"_ins", id_uniq+"_pos", id_uniq+"_upv", id_uniq+"_boo", id_uniq+"_ers", id_uniq+"_ing", id_uniq+"_com", id_uniq + "_vis", id_uniq+"_act", id_uniq+"_tag", country, state, city, website, git, org, True, id_uniq+"_rep", True))
+                        query = "INSERT INTO Personal (id_obj, id_uniq, pass) values (%s, %s, %s)"
+                        cur.execute(query, ('A', id_uniq, hash_password))
+                        query = "INSERT INTO Id (id_obj, id_uniq) values (%s, %s)"
+                        cur.execute(query, ('A', id_uniq))
+                        helper.create_linked_table(cur, id_uniq, "_ins", 'I')
+                        helper.create_linked_table(cur, id_uniq, "_pos", 'P')
+                        helper.create_linked_table(cur, id_uniq, "_upv", 'A')
+                        helper.create_linked_table(cur, id_uniq, "_boo", 'P')
+                        helper.create_linked_table(cur, id_uniq, "_ers", 'A')
+                        helper.create_linked_table(cur, id_uniq, "_ing", 'A')
+                        helper.create_linked_table(cur, id_uniq, "_com", 'C')
+                        helper.create_linked_table(cur, id_uniq, "_vis", 'P')
+                        helper.create_linked_table(cur, id_uniq, "_act", 'IP')
+                        helper.create_linked_table(cur, id_uniq, "_tag", 'T')
+                        helper.create_linked_table(cur, id_uniq, "_rep", 'R')
+                        del otp_signup[email]
+                        cur.close()
+                        mysql.connection.commit()
+                        return redirect("/")
+                    else:
+                        msg = "Invalid OTP"
+                        cur.close()
+                        return render_template("html/signup.html", message1=msg, username=username, email=email, country=country, state=state, city=city, website=website, git=git, org=org, image=image, name=name)
+                else:
+                    msg = "No valid OTP for "+email
+                    cur.close()
+                    return render_template("html/signup.html", message1=msg, username=username, email=email, country=country, state=state, city=city, website=website, git=git, org=org, image=image, name=name)
+            msg = "There was some error please try again"
+            cur.close()
+            return render_template("html/signup.html", message1=msg)
 
 def init_db():
     with app.app_context():
--- backend/environments/templates/js/loader1.js
+++ backend/environments/templates/js/loader1.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $("#inner_rect_loader").animate({
         width: "420px"
     }, 2000);
-    setTimeout(function (){
-        window.location.href = "{{url_for('home')}}";
-      }, 2000);
+    // setTimeout(function (){
+    //     // window.location.href = "{{url_for('home'),username=username}}";
+    //     window.location.href = "{{url_for('profile')}}";
+    //   }, 2000);
 })
