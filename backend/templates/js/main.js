5f195d9984b1b430a036c48248bbc54c09038910
ris04hit <ris04hit@gmail.com>
2023-04-24 14:00:06 +0900

    Merged Search pop up in header

--- backend/templates/js/main.js
+++ backend/templates/js/main.js
@@ -1,8 +1,28 @@
 var next_url = "home.html"
+var search_list = ["taaag1", "tffag2","aatag3","taggg4"];
 
 $(function(){
     $("#header").load("{{url_for('header')}}"); 
   });
+
+
+  // tag pop up
+  function search_click() {
+      if (search_list.length){
+        $("#search_result_rect").show();
+      }
+      var rect_ht = 50 + 41*search_list.length;
+      $("#search_result_rect").css("height",rect_ht.toString().concat("px"));
+      var html = "";
+      var top = -13;
+      for (var count=0; count< search_list.length; count++){
+          top = top + 41;
+          var code = `<div id = "search_text-${count}" class="text4" style = "top : ${top}px; left : 29px">${search_list[count]}</div><rect class="search_line header_search_line" style = "top : ${top+30}px; width : 220px"></rect>`
+          html = html.concat(code);
+        }
+      console.log(html);
+      $("#search_result_rect").html(html);
+    }
 
 $(document).ready(function(){
     var href = document.location.href;
@@ -45,5 +65,14 @@ $(document).ready(function(){
         $(this).css("color", "#FFFAFA");
       }, function(){
         $(this).css("color", "#7F7F7F");
+      })
+
+      $("#rect_search").focus(function(){
+        search_click();
+        $("#search_result_rect").show();
+      })
+
+      $("#rect_search").blur(function(){
+        $("#search_result_rect").hide();
       })
 })
